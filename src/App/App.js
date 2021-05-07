1684295523df7101ac7975015c5302d082730785
fimoefive <brothermj@hotmail.com>
2021-05-07 15:57:23 -0500

    un commented the useEffect function for firebase.auth() in the App.js file. Also imported firebase and firebase/auth in the App.js file.

--- src/App/App.js
+++ src/App/App.js
@@ -1,4 +1,6 @@
 import React, { useState, useEffect } from 'react';
+import firebase from 'firebase/app';
+import 'firebase/auth';
 import NavBar from '../components/NavBar';
 import { getPlayers } from '../helpers/data/playerData';
 import Routes from '../helpers/Routes';
@@ -6,26 +8,27 @@ import './App.scss';
 
 function App() {
   const [players, setPlayers] = useState([]);
+  const [user, setUser] = useState(null);
 
   useEffect(() => {
     getPlayers().then(setPlayers);
   }, []);
 
-  // useEffect(() => {
-  //   firebase.auth().onAuthStateChanged((authed) => {
-  //     if (authed) {
-  //       const userInfoObj = {
-  //         fullName: authed.displayName,
-  //         profileImage: authed.photoURL,
-  //         uid: authed.uid,
-  //         username: authed.email.split('@')[0]
-  //       };
-  //       setUser(userInfoObj);
-  //     } else if (user || user === null) {
-  //       setUser(false);
-  //     }
-  //   });
-  // }, []);
+  useEffect(() => {
+    firebase.auth().onAuthStateChanged((authed) => {
+      if (authed) {
+        const userInfoObj = {
+          fullName: authed.displayName,
+          profileImage: authed.photoURL,
+          uid: authed.uid,
+          username: authed.email.split('@')[0]
+        };
+        setUser(userInfoObj);
+      } else if (user || user === null) {
+        setUser(false);
+      }
+    });
+  }, []);
 
   return (
     <>
