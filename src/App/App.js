184b3817c55dd5f78211e6abc8d7d088f406ea07
fimoefive <brothermj@hotmail.com>
2021-05-07 12:27:21 -0500

    removed Router from the return and the useEffect for firebaseauth. Also removed user, setUser = useState and all use={user} in the return statement.

--- src/App/App.js
+++ src/App/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
-import firebase from 'firebase/app';
-import 'firebase/auth';
+// import { BrowserRouter as Router } from 'react-router-dom';
+// import firebase from 'firebase/app';
+// import 'firebase/auth';
 import NavBar from '../components/NavBar';
 import { getPlayers } from '../helpers/data/playerData';
 import Routes from '../helpers/Routes';
@@ -9,40 +9,37 @@ import './App.scss';
 
 function App() {
   const [players, setPlayers] = useState([]);
-  const [user, setUser] = useState(null);
+  // const [user, setUser] = useState(null);
 
   useEffect(() => {
     getPlayers().then(setPlayers);
   }, []);
 
-  useEffect(() => {
-    firebase.auth().onAuthStateChanged((authed) => {
-      if (authed) {
-        const userInfoObj = {
-          fullName: authed.displayName,
-          profileImage: authed.photoURL,
-          uid: authed.uid,
-          username: authed.email.split('@')[0]
-        };
-        setUser(userInfoObj);
-      } else if (user || user === null) {
-        setUser(false);
-      }
-    });
-  }, []);
+  // useEffect(() => {
+  //   firebase.auth().onAuthStateChanged((authed) => {
+  //     if (authed) {
+  //       const userInfoObj = {
+  //         fullName: authed.displayName,
+  //         profileImage: authed.photoURL,
+  //         uid: authed.uid,
+  //         username: authed.email.split('@')[0]
+  //       };
+  //       setUser(userInfoObj);
+  //     } else if (user || user === null) {
+  //       setUser(false);
+  //     }
+  //   });
+  // }, []);
 
   return (
     <>
       {/* <div className='App'> */}
       <h2>Basketball Roster</h2>
-      <Router>
-        <NavBar user={user} />
-        <Routes
-          user={user}
-          players={players}
-          setPlayers={setPlayers}
-        />
-      </Router>
+      <NavBar />
+      <Routes
+        players={players}
+        setPlayers={setPlayers}
+      />
     </>
   );
 }
